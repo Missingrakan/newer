167f8265faa9de5543e70e9c860e0a949c793a86
Missingrakan <3047451493@qq.com>
2019-05-26 21:48:34 +0800

    更新代码

--- game2/game2/game.c
+++ game2/game2/game.c
@@ -67,34 +67,129 @@ int GetMineCount(char mine[ROWS][COLS],int x,int y)
 
 }
 
+void RemoveMine(char mine[ROWS][COLS], int x, int y)//第一次踩雷 移走
+{
+	mine[x][y] = '0';
+	GetMineCount(mine, x, y);
+	while (1)
+	{
+		int	x1 = rand() % ROW+1;
+		int	y1 = rand() % COL+1;
+		if (mine[x1][y1] != '1' && ((x1 != x) && (y1 != y)))
+		{
+			mine[x1][y1] = '1';
+			break;
+		}
+	}
+}
+
+void Expand(char mine[ROWS][COLS], char show[ROWS][COLS], int x, int y)//展开函数
+{
+	if ((x >= 1) && (y >= 1) && (x <= ROW) && (y <= COL))
+	{
+		if (GetMineCount(mine, x, y) == 0)  //周围没有雷
+		{
+			show[x][y] = ' ';    //置为空白
+			if (show[x - 1][y - 1] == '*')
+			{
+				Expand(mine, show, x - 1, y - 1);
+			}
+			if (show[x - 1][y] == '*')
+			{
+				Expand(mine, show, x - 1, y);
+			}
+			if (show[x - 1][y + 1] == '*')
+			{
+				Expand(mine, show, x - 1, y + 1);
+			}
+			if (show[x][y + 1] == '*')
+			{
+				Expand(mine, show, x, y + 1);
+			}
+			if (show[x + 1][y + 1] == '*')
+			{
+				Expand(mine, show, x + 1, y + 1);
+			}
+			if (show[x + 1][y] == '*')
+			{
+				Expand(mine, show, x + 1, y);
+			}
+			if (show[x + 1][y - 1] == '*')
+			{
+				Expand(mine, show, x + 1, y - 1);
+			}
+			if (show[x][y - 1] == '*')
+			{
+				Expand(mine, show, x, y - 1);
+			}
+		}
+	}
+}
+
 void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 {
 	int x = 0;
 	int y = 0;
-	int win = 0;
-	while (win<row*col-EASY_COUNT)
+	int clear = 0;
+	int minecount = 0;
+	while (clear<row*col-EASY_COUNT)
 	{
 		printf("请选择要排查的坐标:>");
 		scanf("%d%d", &x, &y);
 		if (x >= 1 && x <= row && y >= 1 && y <= col)
 		{
+			
 			if (mine[x][y] == '1')
 			{
-				printf("很遗憾，你被炸死了!\n");
-				DisPlayBoard(mine,row,col);
-				break;
+				////计算x，y周围有几个雷
+				//int count = GetMineCount(mine, x, y);
+				////show[x][y] = count;
+				////此时count为狮子，而答应函数输出的是字符，故应转成字符
+			
+				////展示排查的信息
+				//show[x][y] = count + '0';
+				//DisPlayBoard(show, row, col);
+				//clear++;
+				if (clear == 0)
+				{
+					RemoveMine(mine, x, y);
+					DisPlayBoard(mine,row,col);
+					printf("\n");
+					minecount = GetMineCount(mine, x, y);
+					if (minecount == 0)
+					{
+						show[x][y] = ' ';
+						clear++;
+						Expand(mine, show, x, y);
+						DisPlayBoard(show, row,col);
+					}
+					else
+					{
+						show[x][y] = minecount + '0';
+						DisPlayBoard(show, ROW, COL);
+					}
+				}
+				else
+				{
+					printf("很遗憾，你被炸死了!\n");
+					DisPlayBoard(mine, row, col);
+					break;
+				}
 			}
 			else
 			{
-				//计算x，y周围有几个雷
-				int count = GetMineCount(mine, x, y);
-				//show[x][y] = count;
-				//此时count为狮子，而答应函数输出的是字符，故应转成字符
-			
-				//展示排查的信息
-				show[x][y] = count + '0';
-				DisPlayBoard(show, row, col);
-				win++;
+				minecount = GetMineCount(mine, x, y);
+				if (minecount == 0)
+				{
+					show[x][y] = ' ';
+				}
+				else
+				{
+					show[x][y] = minecount + '0';
+				}
+				clear++;
+				Expand(mine, show, x, y);
+				DisPlayBoard(show, row,col);
 			}
 		}
 		else
@@ -102,7 +197,7 @@ void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 			printf("输入坐标非法，请重新输入!\n");
 		}
 	}
-	if (win == row*col - EASY_COUNT)
+	if (clear == row*col - EASY_COUNT)
 	{
 		printf("恭喜你，扫雷成功!\n");
 		DisPlayBoard(mine, row, col);
