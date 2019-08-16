e4272127243b2c9110af33be3a34882f9118861b
Missingrakan <3047451493@qq.com>
2019-08-16 17:40:59 +0800

    更新代码

--- game1/game1/game.c
+++ game1/game1/game.c
@@ -91,7 +91,7 @@ static int IsFull(char board[ROW][COL], int row, int col)
 {
 	int i = 0;
 	int j = 0;
-	for (i = 0; i <= row; i++)
+	for (i = 0; i < row; i++)
 	{
 		for (j = 0; j < col; j++)
 		{
